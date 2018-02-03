60baeb76ae0ec9769a8353d2f535462a0602c37c
Miromiaosang <869743653@qq.com>
2018-02-03 22:27:01 +0800

    BinarySearchTree Using recursive method

--- algorithms/src/Sort.java
+++ algorithms/src/Sort.java
@@ -148,6 +148,35 @@ public class Sort {
     return j;
   }
 
+  /**
+   * heap sort
+   * @param a
+   */
+  public static void heap(Comparable[] a) {
+    int N = a.length;
+    for (int k = N / 2; k >= 1; k--) {
+      sink(a, k, N);
+    }
+    while (N > 1) {
+      exch(a, 1, N--);
+      sink(a, 1, N);
+    }
+  }
+
+  private static void sink(Comparable[] a, int k, int N) {
+    while (2 * k <= N) {
+      int j = 2 * k;
+      if (j < N && less(j, j + 1)) {
+        j++;
+      }
+      if (!less(k, j)) {
+        break;
+      }
+      exch(a, k, j);
+      k = j;
+    }
+  }
+
   private static boolean less(Comparable v, Comparable w) {
     return v.compareTo(w) < 0;
   }
