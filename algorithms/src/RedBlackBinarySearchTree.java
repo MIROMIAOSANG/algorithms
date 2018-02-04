240aeb1b2eb7145356779ed7619749a31bc2db8c
Miromiaosang <869743653@qq.com>
2018-02-05 00:05:36 +0800

    BinarySearchTree Using recursive method

---
+++ algorithms/src/RedBlackBinarySearchTree.java
@@ -0,0 +1,31 @@
+/**
+ * Red Black Tree
+ */
+public class RedBlackBinarySearchTree<Key extends Comparable<Key>,Value> {
+  private Node root;
+  private static final boolean RED=true;
+  private static final boolean BLACK=false;
+
+  private class Node{
+    Key key;
+    Value value;
+    Node left;
+    Node right;
+    int N;
+    boolean color;
+
+    Node(Key key,Value value,int N,boolean color){
+      this.key=key;
+      this.value=value;
+      this.N=N;
+      this.color=color;
+    }
+  }
+
+  private boolean isRed(Node x){
+    if(x==null){
+      return false;
+    }
+    return x.color==RED;
+  }
+}
