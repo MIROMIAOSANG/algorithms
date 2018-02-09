7254d20045fe3a58088674dfbcb148f7ce96a467
Miromiaosang <869743653@qq.com>
2018-02-09 23:02:56 +0800

    RedBlackTree Insertion

--- algorithms/src/RedBlackBinarySearchTree.java
+++ algorithms/src/RedBlackBinarySearchTree.java
@@ -1,12 +1,14 @@
 /**
  * Red Black Tree
  */
-public class RedBlackBinarySearchTree<Key extends Comparable<Key>,Value> {
-  private Node root;
-  private static final boolean RED=true;
-  private static final boolean BLACK=false;
+public class RedBlackBinarySearchTree<Key extends Comparable<Key>, Value> {
 
-  private class Node{
+  private Node root;
+  private static final boolean RED = true;
+  private static final boolean BLACK = false;
+
+  private class Node {
+
     Key key;
     Value value;
     Node left;
@@ -14,18 +16,89 @@ public class RedBlackBinarySearchTree<Key extends Comparable<Key>,Value> {
     int N;
     boolean color;
 
-    Node(Key key,Value value,int N,boolean color){
-      this.key=key;
-      this.value=value;
-      this.N=N;
-      this.color=color;
+    Node(Key key, Value value, int N, boolean color) {
+      this.key = key;
+      this.value = value;
+      this.N = N;
+      this.color = color;
     }
   }
 
-  private boolean isRed(Node x){
-    if(x==null){
+  private boolean isRed(Node x) {
+    if (x == null) {
       return false;
     }
-    return x.color==RED;
+    return x.color == RED;
+  }
+
+  private int size() {
+    return size(root);
+  }
+
+  public int size(Node x) {
+    if (x == null) {
+      return 0;
+    } else {
+      return x.N;
+    }
+  }
+
+  private Node rotateLeft(Node h) {
+    Node x = h.right;
+    h.right = x.left;
+    x.left = h;
+    x.color = h.color;
+    h.color = RED;
+    x.N = h.N;
+    h.N = 1 + size(h.left) + size(h.right);
+    return x;
+  }
+
+  private Node rotateRight(Node h) {
+    Node x = h.left;
+    h.left = x.right;
+    x.right = h;
+    x.color = h.color;
+    h.color = RED;
+    x.N = h.N;
+    h.N = 1 + size(h.left) + size(h.right);
+    return x;
+  }
+
+  private void flipColors(Node h) {
+    h.color = RED;
+    h.left.color = BLACK;
+    h.right.color = BLACK;
+  }
+
+  public void put(Key key, Value value) {
+    root = put(root, key, value);
+    root.color = BLACK;
+  }
+
+  private Node put(Node h, Key key, Value value) {
+    if (h == null) {
+      return new Node(key, value, 1, RED);
+    }
+    int cmp = key.compareTo(h.key);
+    if (cmp < 0) {
+      h.left = put(h.left, key, value);
+    } else if (cmp > 0) {
+      h.right = put(h.right, key, value);
+    } else {
+      h.value = value;
+    }
+
+    if (isRed(h.right) && !isRed(h.left)) {
+      h = rotateLeft(h);
+    }
+    if (isRed(h.left) && !isRed(h.right)) {
+      h = rotateRight(h);
+    }
+    if (isRed(h.left) && isRed(h.right)) {
+      flipColors(h);
+    }
+    h.N=size(h.left)+size(h.right)+1;
+    return h;
   }
 }
