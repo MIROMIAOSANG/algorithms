363954c91890bccf8e9ded14cfe3cf5e8b8fdb3f
Miromiaosang <869743653@qq.com>
2018-02-11 23:15:19 +0800

    Review OOP

---
+++ algorithms/src/JavaDemo/OOPDemo.java
@@ -0,0 +1,16 @@
+package JavaDemo;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * java oop grammar
+ */
+public class OOPDemo {
+
+  public static void main(String[] args) {
+    System.out.println(new Date().toString());
+    System.out.println(LocalDate.now());
+
+  }
+}
