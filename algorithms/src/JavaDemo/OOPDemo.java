fb1fe2a071c485e24a4c9f0a8f114fb41ea98b2e
Miromiaosang <869743653@qq.com>
2018-02-12 13:47:53 +0800

    test

--- algorithms/src/JavaDemo/OOPDemo.java
+++ algorithms/src/JavaDemo/OOPDemo.java
@@ -11,6 +11,7 @@ public class OOPDemo {
   public static void main(String[] args) {
     System.out.println(new Date().toString());
     System.out.println(LocalDate.now());
-
+//    System.out.println(1/0);
+    System.out.println(1.0/0);
   }
 }
